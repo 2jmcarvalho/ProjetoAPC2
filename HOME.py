9bdd78564ec35f670029e124c56f05a9cd33f241
João Marcos S. Carvalho <122795840+2jmcarvalho@users.noreply.github.com>
2024-05-18 11:41:35 -0300

    Add files via upload

---
+++ HOME.py
@@ -0,0 +1,92 @@
+import os
+import streamlit as st
+import sqlite3
+import streamlit_authenticator as stauth
+
+def exibir_playlist_youtube(playlist, termo_busca):
+    st.write("Playlist do YouTube:")
+    for video in playlist:
+        if termo_busca.lower() in video['titulo'].lower():
+            #st.markdown(f"[{video['titulo']}]({video['link']})")
+            st.video(video['link'])
+
+
+
+
+
+    
+    
+
+st.set_page_config(layout="wide", page_title="Guia do Universitario", page_icon="🤓")
+st.title("Guia do Universitario")
+
+def register(username, password):
+    with open("user_credentials.txt", "a") as file:
+        file.write(f"{username}:{password}\n")
+
+def login(username, password):
+    with open("user_credentials.txt", "r") as file:
+        for line in file:
+            stored_username, stored_password = line.strip().split(":")
+            if username == stored_username and password == stored_password:
+                return True
+    return False
+
+#def main():
+    st.title("Login e Registro")
+
+    username = st.text_input("Nome de usuário")
+    password = st.text_input("Senha", type="password")
+
+
+    if st.button("Registrar"):
+        if len(username) == 0 or len(password) == 0:
+            st.warning("Por favor, preencha todos os campos.")
+        else:
+            register(username, password)
+            st.success("Registro realizado com sucesso. Você pode fazer login agora.")
+    if st.button("Login"):
+        if len(username) == 0 or len(password) == 0:
+            st.warning("Por favor, preencha todos os campos.")
+        else:
+            if os.path.exists("user_credentials.txt"):
+                if login(username, password):
+                    st.success("Login bem-sucedido!")
+                    st.empty()
+                    next_page(username)  # Chamada para a próxima página após o login
+                else:
+                    st.error("Credenciais inválidas.")
+            else:
+                st.error("Nenhum usuário registrado. Por favor, registre-se primeiro.")
+
+
+def main():
+    #st.title(f"Bem-vindo, {username}!")
+    tab1, tab2, tab3 = st.tabs(["Inicio", "Créditos", "Monitoria(em breve)"])
+
+    with tab1:
+        st.subheader("canais recomendados")
+        st.markdown(f'<a href="{"https://www.youtube.com/@CanalUSP"}"><img src="{"https://yt3.googleusercontent.com/hwpjVSGYUvTWn6wtXJ_75BWdwG_Z_erSU-gM3XfsqzzHI51QPnKErtC5iJEshXc86T7nNTh5tq0=s160-c-k-c0x00ffffff-no-rj"}" width="200"></a>', unsafe_allow_html=True)
+        st.subheader("videos recomendados")
+        playlist = [
+        {"titulo": "Vídeo 1", "link": "https://www.youtube.com/watch?v=WgHUHPlJETs&list=PLAudUnJeNg4tr-aiNyYCXE46L3qEZ2Nzx"},
+        {"titulo": "Vídeo gaga", "link": "https://www.youtube.com/watch?v=qrO4YZeyl0I"},
+        {"titulo": "Vídeo 3", "link": "https://www.youtube.com/watch?v=WgHUHPlJETs&list=PLAudUnJeNg4tr-aiNyYCXE46L3qEZ2Nzx"}
+    ]
+        termo_busca = st.text_input("Digite o título do vídeo:")
+        #exibir_playlist_youtube(playlist, termo_busca)
+
+        s1, s2, s3, s4, s5 = st.columns(5)
+        with s1:
+            exibir_playlist_youtube(playlist, termo_busca)
+            st.video("https://www.youtube.com/watch?v=WgHUHPlJETs&list=PLAudUnJeNg4tr-aiNyYCXE46L3qEZ2Nzx")
+        with s2:
+            exibir_playlist_youtube(playlist, termo_busca)
+            st.video("https://www.youtube.com/watch?v=nKua7iJK2WQ&list=PLAudUnJeNg4tr-aiNyYCXE46L3qEZ2Nzx&index=2")
+
+
+    
+
+
+if __name__ == "__main__":
+    main()
