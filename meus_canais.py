327b30a509609150ff5b2cd60f33dd305628ed32
João Marcos S. Carvalho <122795840+2jmcarvalho@users.noreply.github.com>
2024-06-20 08:56:13 -0300

    Create meus_canais.py

---
+++ meus_canais.py
@@ -0,0 +1,11 @@
+canais = [
+        {"titulo": "Curso em Vídeo", "link": "https://www.youtube.com/c/CursoemV%C3%ADdeo" , "imagem": "https://yt3.googleusercontent.com/ytc/AIdro_m-8zZRJBWMwnaHcJgQv2IRr0dLcqCv1yKcQHgSEZ26jHw=s160-c-k-c0x00ffffff-no-rj"},
+        {"titulo": "Help Engenharia com Prof. Dênis Rodrigues", "link": "https://www.youtube.com/@HelpEngenharia" , "imagem": "https://yt3.googleusercontent.com/68JRWeh5ZsvALQXATU2eeeYmIHRLpYnWChBcVc8AWlpi_Xsm_kaJfcRTlnE8-33cGUBj753cbLQ=s160-c-k-c0x00ffffff-no-rj"},
+        {"titulo": "Professor Douglas Maioli", "link": "https://www.youtube.com/@ProfessorDouglasMaioli" , "imagem": "https://yt3.googleusercontent.com/ytc/AIdro_kMosdggxqGD_i0OM_YFvY6bhqxJMqGQS-L5gyTLN1CIQ=s160-c-k-c0x00ffffff-no-rj"},
+        {"titulo": "Canal USP", "link": "https://www.youtube.com/@CanalUSP" , "imagem": "https://yt3.googleusercontent.com/hwpjVSGYUvTWn6wtXJ_75BWdwG_Z_erSU-gM3XfsqzzHI51QPnKErtC5iJEshXc86T7nNTh5tq0=s160-c-k-c0x00ffffff-no-rj"},
+        {"titulo": "Matemateca - Ester Velasquez", "link": "https://www.youtube.com/@Matemateca" , "imagem": "https://yt3.googleusercontent.com/12dY4NJeAKazcLr5kQ_oySHdDYHjzOorD6HnmoWaiONjfB_KPjMdfYFXdIQyCB5QXQ7z745JQg=s160-c-k-c0x00ffffff-no-rj"},
+        {"titulo": "Hashtag Programação", "link": "https://www.youtube.com/c/HashtagPrograma%C3%A7%C3%A3o" , "imagem": "https://yt3.googleusercontent.com/-kPp7Q9HemvXXn4yK8wbH9KFAvNeUTD6cCh1kmJRFE7GLtpdzIDhegtEfhcpa10xUyeKZUATvg=s160-c-k-c0x00ffffff-no-rj"},
+
+    ]
+
+#{"titulo": "", "link": "" , "imagem": ""},
