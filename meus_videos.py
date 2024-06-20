9f695a82fbeccc5f0b2395222992ab9b5817730d
João Marcos S. Carvalho <122795840+2jmcarvalho@users.noreply.github.com>
2024-06-20 08:56:57 -0300

    Create meus_videos.py

---
+++ meus_videos.py
@@ -0,0 +1,11 @@
+videos = [
+        {"titulo": "Cálculo I - Um pouco de História e Aplicações do Cálculo", "link": "https://www.youtube.com/watch?v=EJ5FGPZbKeo&list=PLrOyM49ctTx8go5KFpSr-EMScIPygZNob"},
+        {"titulo": "Cálculo I - Revisão de Funções", "link": "https://www.youtube.com/watch?v=ruc5eKGbzak&list=PLrOyM49ctTx8go5KFpSr-EMScIPygZNob&index=3"},
+        {"titulo": "Cálculo I - Noção Intuitiva de Limites (Parte I de Limites)", "link": "https://www.youtube.com/watch?v=ZxmuBZHiH90&list=PLrOyM49ctTx8go5KFpSr-EMScIPygZNob&index=4"},
+        {"titulo": "Cálculo I - Definição Formal e Precisa de Limite (Parte II de Limites)", "link": "https://www.youtube.com/watch?v=qLmanGH1WBw&list=PLrOyM49ctTx8go5KFpSr-EMScIPygZNob&index=5"},
+        {"titulo": "Cálculo I - Substituição Direta e Limites Indeterminados (Parte III de Limites)", "link": "https://www.youtube.com/watch?v=bjr_pGNWdGE"},
+        {"titulo": "Cálculo I - Limites no Infinito (Parte IV de Limites)", "link": "https://www.youtube.com/watch?v=2erMaIAp0Jk"},
+        {"titulo": "Cálculo I - Limites Laterais (Parte V de Limites)", "link": "https://www.youtube.com/watch?v=l_WK_mdO6lA"},
+        {"titulo": "Cálculo I - Limites Infinitos (Parte VI de Limites)", "link": "https://www.youtube.com/watch?v=JXgjnxEuJMY"},
+        {"titulo": "Cálculo I - Continuidade (Parte VII de Limites)", "link": "https://www.youtube.com/watch?v=Xlky0dc8KFM"},
+    ]   
